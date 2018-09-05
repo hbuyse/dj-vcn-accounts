3ea6ea941bf192561361b429b34b54bc3df09199
Henri Buyse <henri.buyse@gmail.com>
2018-09-05 17:17:52 +0200

    Disable logging when testing

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+import logging
 import os
 import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     from django.core.management import execute_from_command_line
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        logging.disable(logging.CRITICAL)
 
     execute_from_command_line(sys.argv)
