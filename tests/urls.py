d48a6e4d3d806c127fc2a54d5653cf1e4d715975
Henri Buyse <henri.buyse@gmail.com>
2019-01-08 19:43:47 +0100

    Update test urls

--- tests/urls.py
+++ tests/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('login', auth_views.login, name='vcn-account-login'),
-    path('logout', auth_views.logout, {'next_page': '/'}, name='vcn-account-logout'),
+    path('login', auth_views.LoginView.as_view(), name='vcn-account-login'),
+    path('logout', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='vcn-account-logout'),
     path('', include('dj_vcn_accounts.urls', namespace='dj_vcn_accounts')),
 ]
